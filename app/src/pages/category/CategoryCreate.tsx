73419cb4da68326cd4dede23e55843b672dcede1
Denis Kruschinski <4989565+kruschid@users.noreply.github.com>
2025-11-21 17:11:35 +0100

    app implements category create and edit

--- app/src/pages/category/CategoryCreate.tsx
+++ app/src/pages/category/CategoryCreate.tsx
@@ -1,1 +1,49 @@
-export const CategoryCreate = () => <></>;
+import { TextInput } from "@mantine/core";
+import { type HttpError, useTranslate } from "@refinedev/core";
+import { Create, useForm } from "refine-mantine";
+import type { CategoryRecord } from "../../pocketbase.generated";
+
+type CategoryFormValues = Required<Pick<
+  CategoryRecord,
+  "title"
+>>;
+
+export const useCategoryForm = () => {
+  const {
+    saveButtonProps,
+    getInputProps,
+    refineCore: { formLoading },
+  } = useForm<CategoryRecord, HttpError, CategoryFormValues>({
+    initialValues: {
+      title: ""
+    },
+  });
+
+  return {
+    formLoading,
+    saveButtonProps,
+    getInputProps,
+  }
+}
+
+export const CategoryForm = (p: ReturnType<typeof useCategoryForm>) => {
+  const t = useTranslate();
+
+  return (
+    <TextInput
+      label={t("pages.category.categoryForm.titleLabel", "Title")}
+      placeholder={t("pages.category.categoryForm.titlePlaceholder", "Please enter a title")}
+      {...p.getInputProps("title")}
+    />
+  );
+}
+
+export const CategoryCreate = () => {
+  const formProps = useCategoryForm();
+
+  return (
+    <Create saveButtonProps={formProps.saveButtonProps}>
+      <CategoryForm {...formProps}/>
+    </Create>
+  );
+}
--- app/src/pages/category/CategoryEdit.tsx
+++ app/src/pages/category/CategoryEdit.tsx
@@ -1,1 +1,12 @@
-export const CategoryEdit = () => <></>;
+import { Edit } from "refine-mantine";
+import { CategoryForm, useCategoryForm } from "./CategoryCreate";
+
+export const CategoryEdit = () => {
+  const formProps = useCategoryForm();
+
+  return (
+    <Edit saveButtonProps={formProps.saveButtonProps}>
+      <CategoryForm {...formProps}/>
+    </Edit>
+  );
+}
